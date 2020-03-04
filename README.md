4289dc7ad534300b9850d0891b94899f261d1f94
Alexander Menne <alexander.menne@amenne.de>
2020-03-04 10:56:12 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,19 @@
+# Project Keep is a miniature painter and collectors dream!
+
+tbd
+
+## Toolchains
+
+tbd
+
+## Features
+
+tbd
+
+## Support
+
+If you like this repo and want to give something back, you can help me grab a coffee:
+
+[![ko-fi](https://www.ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/B0B11826R)
+
+Thank you!
---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
